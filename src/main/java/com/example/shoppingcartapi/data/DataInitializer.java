7e3bfbff6f6eb2219285b387e8043ff33273f1e4
Min Khant <djidhdh2558@gmail.com>
2026-06-05 22:07:14 +0630

    refactor: optimize DataInitializer for production readiness

--- src/main/java/com/example/shoppingcartapi/data/DataInitializer.java
+++ src/main/java/com/example/shoppingcartapi/data/DataInitializer.java
@@ -5,7 +5,8 @@ import com.example.shoppingcartapi.entity.User;
 import com.example.shoppingcartapi.repository.RoleRepository;
 import com.example.shoppingcartapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -13,71 +14,52 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
-@Transactional
+
 @RequiredArgsConstructor
 @Component
-public class DataInitializer implements ApplicationListener {
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
     @Override
-    public void onApplicationEvent(ApplicationEvent event) {
-        Set<String> defaultRole = Set.of("ROLE_ADMIN", "ROLE_USER");
-        createDefaultUserIfNotExist();
-        createDefaultRoleIfNotExist(defaultRole);
+    @Transactional
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
+        createDefaultRoleIfNotExist(defaultRoles);
         createDefaultAdminIfNotExist();
-    }
-
-    private void createDefaultUserIfNotExist() {
-        Role userRole = roleRepository.findByName("ROLE_USER");
-        for (int i = 1; i <= 5; i++) {
-            String defaultEmail = "user"+i+"@email.com";
-            if (userRepository.existsByEmail(defaultEmail)) {
-                continue;
-            }
-            User user = new User();
-            user.setFirstName("The User");
-            user.setLastName("User" + i);
-            user.setPassword(passwordEncoder.encode("12345"));
-            user.setEmail(defaultEmail);
-            user.setRoles(Set.of(userRole));
-            userRepository.save(user);
-            System.out.println("Default user " + i + " created successfully");
-        }
-    }
-
-    private void createDefaultAdminIfNotExist() {
-        Role adminRole = roleRepository.findByName("ROLE_ADMIN");
-        for (int i = 1; i <= 5; i++) {
-            String defaultEmail = "admin"+i+"@email.com";
-            if (userRepository.existsByEmail(defaultEmail)) {
-                continue;
-            }
-            User user = new User();
-            user.setFirstName("Admin");
-            user.setLastName("Admin" + i);
-            user.setPassword(passwordEncoder.encode("12345"));
-            user.setEmail(defaultEmail);
-            user.setRoles(Set.of(adminRole));
-            userRepository.save(user);
-            System.out.println("Default admin user " + i + " created successfully");
-
-        }
     }
 
     private void createDefaultRoleIfNotExist(Set<String> roles) {
         roles.forEach(roleName -> {
-            // Check if the role already exists in the DB
             Role existingRole = roleRepository.findByName(roleName);
-
-            // If it doesn't exist, create and save it
             if (existingRole == null) {
                 Role newRole = new Role(roleName);
                 roleRepository.save(newRole);
                 System.out.println("Default role " + roleName + " created successfully");
             }
         });
+    }
+
+    private void createDefaultAdminIfNotExist() {
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN");
+
+        if (!userRepository.existsByEmail(adminEmail)) {
+            User user = new User();
+            user.setFirstName("Admin");
+            user.setLastName("2");
+            user.setPassword(passwordEncoder.encode(adminPassword));
+            user.setEmail(adminEmail);
+            user.setRoles(Set.of(adminRole));
+            userRepository.save(user);
+            System.out.println("Default admin user created successfully");
+        }
     }
 }
