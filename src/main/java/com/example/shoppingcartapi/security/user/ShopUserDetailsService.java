86d558e0052a0051ac39d1d8aa39dfae5ec22a9b
Min Khant <djidhdh2558@gmail.com>
2026-06-05 18:11:41 +0630

    fix(security): resolve NullPointerException in ShopUserDetailsService

--- src/main/java/com/example/shoppingcartapi/security/user/ShopUserDetailsService.java
+++ src/main/java/com/example/shoppingcartapi/security/user/ShopUserDetailsService.java
@@ -8,16 +8,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class ShopUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = Optional.ofNullable(userRepository.findByEmail(email))
+
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
         return ShopUserDetails.buildUserDetails(user);
