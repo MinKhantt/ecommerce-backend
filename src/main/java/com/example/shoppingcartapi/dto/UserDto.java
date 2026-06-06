981ca5a03a449efc0c73b0e6f5d299f940a382c3
Min Khant <djidhdh2558@gmail.com>
2026-06-06 22:46:57 +0630

    fix: remove cart and order from UserDto

--- src/main/java/com/example/shoppingcartapi/dto/UserDto.java
+++ src/main/java/com/example/shoppingcartapi/dto/UserDto.java
@@ -16,4 +16,6 @@ public class UserDto implements Serializable {
     private String firstName;
     private String lastName;
     private String email;
+    private CartDto cart;
+    private List<OrderSummaryDto> orders;
 }
