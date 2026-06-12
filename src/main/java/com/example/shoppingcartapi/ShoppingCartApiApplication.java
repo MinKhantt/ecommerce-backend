403d7dc8105f06987819c992b506653d0f2b6eb6
Min Khant <djidhdh2558@gmail.com>
2026-06-12 21:37:15 +0630

    refactor: remove enable caching annotation

--- src/main/java/com/example/shoppingcartapi/ShoppingCartApiApplication.java
+++ src/main/java/com/example/shoppingcartapi/ShoppingCartApiApplication.java
@@ -2,11 +2,11 @@ package com.example.shoppingcartapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableCaching
+@EnableAsync
 @EnableJpaAuditing
 public class ShoppingCartApiApplication {
 
